4060d894353d74b25fe5e29b4e8ad303cfc71f95
BPachecoAlfaro <bpachecoalfaro@gmail.com>
2023-11-09 19:08:36 -0300

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,22 @@
+import fs from 'fs'
 
-const message = 'Hola Mundo!';
+let outputMessage = '';
+const tableOf = 5;
+const headerMessage = `
+================================
+    Tabla del ${ tableOf }
+================================\n
+`
 
-console.log( message );
+for (let index = 1; index <= 10; index++) {
+    outputMessage += (`${ tableOf } x ${ index } = ${ tableOf * index }\n`);
+}
+
+outputMessage = headerMessage + outputMessage
+console.log( outputMessage )
+
+const outputPath = 'outputs'
+
+fs.mkdirSync( outputPath, { recursive: true})
+fs.writeFileSync(`${ outputPath}/tabla-${ tableOf }.txt`, outputMessage)
+console.log('File was created');
