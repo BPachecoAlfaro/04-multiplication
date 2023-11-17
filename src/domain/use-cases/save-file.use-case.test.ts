568c0f376c7d0b607faea7b6a800263eea9f1c98
BPachecoAlfaro <bpachecoalfaro@gmail.com>
2023-11-17 20:10:08 -0300

    save-file test terminados

--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -1,26 +1,34 @@
-import { afterEach } from 'node:test';
 import { SaveFile } from './save-file.use-case';
 import fs from 'fs';
 
 describe('save-file.use-case', () => {
-
+    
+    
     // beforeEach( () => {
-    //     // clean up
-    //     fs.rmSync('outputs', { recursive: true })
+    //     const outputFolderExists = fs.existsSync('outputs');
+    //     if ( outputFolderExists ) fs.rmSync('outputs', { recursive: true });
+        
     // });
 
+    
     afterEach(() => {
-        fs.rmSync('outputs', { recursive: true });
+        // const outputFolderExists = fs.existsSync('outputs');
+        // if ( outputFolderExists ) fs.rmSync('outputs', { recursive: true });
+
+        const customOutputFolderExists = fs.existsSync('custom-outputs');
+        if ( customOutputFolderExists ) fs.rmSync('custom-outputs', { recursive: true });
     });
 
-    test('should save file with defauilt values', () => {
 
+    
+    test('should save file with defauilt values', () => {
+        
         const saveFile = new SaveFile();
         const filePath = 'outputs/table.txt'
         const options = {
             fileContent: 'test content'
         };
-
+        
         const result = saveFile.execute( options );
         
         const fileExist = fs.existsSync( filePath );
@@ -29,7 +37,27 @@ describe('save-file.use-case', () => {
         expect( result ).toBe( true );
         expect( fileExist ).toBe(true);
         expect( fileContent ).toBe( options.fileContent );
-
+        
     });
-
+    
+    test('should save file with custom values', () => {
+        
+        const saveFile = new SaveFile();
+        const customOptions = {
+            fileContent: 'Custom content',
+            fileDestination: 'custom-outputs',
+            fileName: 'custom-table-name',
+        };
+        
+        const filePath = `${customOptions.fileDestination}/${customOptions.fileName}.txt`
+        
+        const result = saveFile.execute( customOptions );
+        const fileExist = fs.existsSync( filePath );
+        const fileContent = fs.readFileSync( filePath , { encoding: 'utf8'});
+        
+        expect( result ).toBe( true );
+        expect( fileExist ).toBe(true);
+        expect( fileContent ).toBe( customOptions.fileContent );
+        
+    });
 });
